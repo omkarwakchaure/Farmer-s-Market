733df13b5d02e4d5ee1cb147121ed715d7af6b65
Omkar <omkar@wakchaure.com>
2024-04-12 01:27:00 +0530

    complete sign in page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -56,7 +56,7 @@ export default function SignUp(){
       className="border p-3 rounded-lg" id="password" onChange={handleChange}/>
     <button disabled={loading} className='bg-slate-700 text-white p-3 
     rounded-lg uppercase hover:opacity-95
-    disabled:opacity-80'>SIGN UP</button>
+    disabled:opacity-80'>{loading ? 'Loading...' : 'Sign Up'}</button>
     </form>
     <div className="flex gap-2 mt-5">
       <p>Have an account?</p>
